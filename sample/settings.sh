abb7bd24ba5711e766ab77585e2676b1299d8e00
Alejandro Liu <alejandro_liu@hotmail.com>
2019-04-01 12:01:40 +0200

    docs..

--- sample/settings.sh
+++ sample/settings.sh
@@ -22,3 +22,13 @@ XID_MAX=65500
 
 GID_USERS=11000
 GNAME_USERS=users
+
+#~ if [ -f $TLR_HOME/site-settings.sh ] ; then
+  #~ . $TLR_HOME/site-settings.sh
+#~ fi
+
+# Examples
+#
+# add_role host1 role-a role-b role-c
+# add_role host2 host3 host4 : role-a role-b role-c
+# add_host role-a host1 host2 host3
