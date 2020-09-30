811adcd62eee605ebaf2abaec8972031bbb29e33
Alejandro Liu <alejandro_liu@hotmail.com>
2020-09-30 23:47:07 +0200

    Implemented hostmgr

---
+++ tests/60-hostmgr.t.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# starting point for tests
+#
+. ${TESTDIR:=$(cd $(dirname "$0") ; pwd)}/common.sh
+export TLR_DATA=$TESTDIR/t.data/tlr
+
+hostmgr && quit 1 "Should fail!" || :
+hostmgr error && quit 1 "Should fail!" || :
+
+hostmgr list
+
+hostmgr del new1
+hostmgr new --base64 new1
+hostmgr new --base64 new1 && quit 1 "should fail!" || :
+hostmgr new --clobber new1 | tar ztvf -
+
+hostmgr del new1
+
+hostmgr list --pub sys1
+hostmgr list --pub -v sys1
+
+hostmgr set sys1 potone p1
+hostmgr get sys1 potone
+hostmgr get -v sys1 potone
+
