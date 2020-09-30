eb2e3ce34ca5a1d3c698fadbfedcb1de01006b9c
Alejandro Liu <alejandro_liu@hotmail.com>
2020-09-30 23:43:03 +0200

    Updated software install

--- snippets.sh
+++ snippets.sh
@@ -8,6 +8,70 @@ prepare_dist() {
   tar --exclude-vcs -cf - -C $mydir/base . | tar -xf - -C $w
 }
 
+ii_update="Update/install software"
+ll_update() {
+  local w=$(mktemp -d) rc=0
+  (
+    rcmd '[ -f /etc/tlr.cfg ] && . /etc/tlr.cfg'
+    rcmd '[ -z "${TLR_BASE:-}" ] && export TLR_BASE="/usr/local/lib/tlr"'
+
+    # OK, unpack to an adjacent directory....
+    rcmd 'tm=$(mktemp -p $(dirname $TLR_BASE) -d)'
+    prepare_dist "$w/tlr"
+    rcmd '(base64 -d | tar -C $tm -zxvf - ) <<_EOF_'
+    tar --exclude-vcs -zcf - -C "$w" tlr | sshout base64
+    rcmd ''
+    rcmd '_EOF_'
+
+    # Replace old with new...
+    rcmd '[ -d $TLR_BASE ] && mv $TLR_BASE $tm/old'
+    rcmd 'mv $tm/tlr $TLR_BASE'
+
+    rcmd 'rm -rf $tm'
+  )  || rc=$?
+  rm -rf "$w"
+
+ return $rc
+}
+pl_update() {
+  echo BASEDIR=${TLR_BASE:=/usr/local/lib/tlr}
+  echo BINDIR= ${TLR_BIN:=$(dirname $(dirname "$TLR_BASE"))/bin}
+  local tag="ooGhof7pCai4giedAenie1ooih1CheexahM8nah3"
+
+  find $TLR_BASE/bin -maxdepth 1 -type f | (
+    m="" q=""
+    while read f
+    do
+      local cmd=$(basename "$f") args='"$@"'
+      [ -L "$TLR_BIN/$cmd" ] && rm "$TLR_BIN/$cmd"
+
+      ( fixfile --mode=755 "$TLR_BIN/$cmd" || : ) <<-_EOF_
+	#!/bin/sh
+	# $tag
+	exec $f $args
+	_EOF_
+      m="$m$q$cmd" ; q=" "
+    done
+    echo "$m"
+    find $TLR_BIN -maxdepth 1 -type f | (
+      while read f
+      do
+        cmd=$(basename "$f")
+	for z in $m ""
+	do
+	  [ -z "$z" ] && break
+	  [ $z = $cmd ] && break
+	done
+	[ -n "$z" ] && continue
+	if grep -q "$tag" "$f" ; then
+	  # matched
+	  rm -v "$f"
+	fi
+      done
+    )
+  )
+
+}
 
 
 ii_try="Try stuff out"
