fa289c2d0732d09cb996c4b2fb6e08a8f4c80a64
Alejandro Liu <alejandro_liu@hotmail.com>
2020-09-30 23:43:52 +0200

    minor bugfixes

--- tests/common.sh
+++ tests/common.sh
@@ -3,12 +3,13 @@
 # Prepare settings for tests
 #
 
-export SRCDIR=$TESTDIR/..
+export SRCDIR=$(cd $TESTDIR/.. && pwd)
 
 export \
 	TLR_CFG=$TESTDIR/tlr.cfg \
 	TLR_BASE=$SRCDIR/base \
-	ASHLIB=$SRCDIR/.ashlib
+	ASHLIB=$SRCDIR/.ashlib \
+	PATH=$SRCDIR/base/bin:$PATH
 
 #~ [ -d $TESTDIR/t.data ] && export TLR_DATA=$TESTDIR/t.data
 
